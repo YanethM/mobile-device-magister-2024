b3aaa4cfe2e28b08d81106301e22a0c83c1d88d3
YanethM <yaneth.mejiar@autonoma.edu.co>
2024-03-22 18:30:38 -0500

    Add the getUsers function in server

--- MyFirstApp/src/screens/HomeScreen.tsx
+++ MyFirstApp/src/screens/HomeScreen.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import axios from "axios";
 
 export const HomeScreen = () => {
+  const navigation = useNavigation();
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get("http://192.168.1.9:3000/api/v1/users");
+      console.log(response.data);
+      setUsers(response.data);
+    } catch (error) {
+      console.log("Error", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Home!</Text>
